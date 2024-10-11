6ac4d57d0428b9613fa812db99f3233ea4d16898
Thirawat001 <thirawat25467@gmail.com>
2024-10-11 16:39:49 +0700

    edit

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -26,7 +26,9 @@ export default function MyCard(props) {
     </Typography>
   </CardContent>
   <CardActions>
-    <Button size="small">Learn More</Button>
+    <Button size="small">xxxxxxxxxxxxxxxx</Button>
+        <Button size="small">Learn More</Button>
+
   </CardActions>
 </Card>
 
